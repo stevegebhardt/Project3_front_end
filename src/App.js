caf89a9a1e677219552be5aace1db43dc396fd01
Steven Gebhardt <stevengebhardt@stevens-mbp.lan>
2023-03-29 13:55:08 -0400

    updating axios routes to heroku url to test

--- src/App.js
+++ src/App.js
@@ -11,23 +11,34 @@ function App() {
   const [showEdit, setShowEdit] = useState(false);
 
   const getRestaurants = () => {
-    axios.get("http://localhost:3000/restaurants").then((response) => {
-      setRestaruants(response.data);
-    });
+    axios
+      .get("https://git.heroku.com/sparklyunicornmagicapp.git/restaurants")
+      .then((response) => {
+        setRestaruants(response.data);
+      });
   };
 
   const handleCreate = (data) => {
-    axios.post("http://localhost:3000/restaurants", data).then((response) => {
-      console.log(response);
-      let newRestaurants = [...restaurants, response.data];
-      setRestaruants(newRestaurants);
-      console.log(newRestaurants);
-    });
+    axios
+      .post(
+        "https://git.heroku.com/sparklyunicornmagicapp.git/restaurants",
+        data
+      )
+      .then((response) => {
+        console.log(response);
+        let newRestaurants = [...restaurants, response.data];
+        setRestaruants(newRestaurants);
+        console.log(newRestaurants);
+      });
   };
 
   const handleEdit = (data) => {
     axios
-      .put("http://localhost:3000/restaurants/" + data._id, data)
+      .put(
+        "https://git.heroku.com/sparklyunicornmagicapp.git/restaurants/" +
+          data._id,
+        data
+      )
       .then((response) => {
         console.log(response);
         let newRestaurants = restaurants.map((restaurant) => {
@@ -40,7 +51,10 @@ function App() {
   const handleDelete = (deletedRestaurant) => {
     console.log(deletedRestaurant._id);
     axios
-      .delete("http://localhost:3000/restaurants/" + deletedRestaurant._id)
+      .delete(
+        "https://git.heroku.com/sparklyunicornmagicapp.git/restaurants/" +
+          deletedRestaurant._id
+      )
       .then((response) => {
         let newRestaurants = restaurants.filter((restaurant) => {
           return restaurant._id !== deletedRestaurant._id;
