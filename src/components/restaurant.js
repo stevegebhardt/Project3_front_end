82120699969be3bcbdbcfedc1e5629e4603f42f8
Steven Gebhardt <stevengebhardt@stevens-mbp.lan>
2023-03-29 11:32:35 -0400

    comitting before branching

--- src/components/restaurant.js
+++ src/components/restaurant.js
@@ -1,21 +1,17 @@
-//need to figure out how to display them !
-//git isnt allowing me to push{test}
-
-const Restaurant = (props) =>{ 
-    return(
-        <>
-        <div class="container">
-            <div>
-                <img src={props.restaurant.restImg}/>
-            </div>
-            <h3>{props.restaurant.restName}</h3>
-            <p>{props.restaurant.price}</p>
-            <p>{props.restaurant.ranking}</p>
-            <p>{props.restaurant.adress}</p>
+const Restaurant = (props) => {
+  return (
+    <>
+      <div class="container">
+        <div>
+          <img src={props.restaurant.restImg} />
         </div>
-        
-        </>
-    )
-}
+        <h3>{props.restaurant.restName}</h3>
+        <p>{props.restaurant.price}</p>
+        <p>{props.restaurant.ranking}</p>
+        <p>{props.restaurant.adress}</p>
+      </div>
+    </>
+  );
+};
 
-export default Restaurant 
+export default Restaurant;
