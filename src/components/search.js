38e0914c6668d110124b18eb1d2996e4b1a530db
Steven Gebhardt <stevengebhardt@stevens-mbp.lan>
2023-04-02 14:50:09 -0400

    back and forth

--- src/components/search.js
+++ src/components/search.js
@@ -1,40 +1,62 @@
-import {useState} from 'react'
-import "./searchbar.css"
+import { useState } from "react";
+import "./searchbar.css";
 
-const Search = ({placeholder, data}) =>{
+const Search = ({ placeholder, data }) => {
+  const [filteredData, setFilteredData] = useState([]);
 
-    const [filteredData, setFilteredData] = useState([])
+  const handleFilter = (event) => {
+    const searchWord = event.target.value;
+    const newFilter = data.filter((value) => {
+      return value.city.toLowerCase().includes(searchWord.toLowerCase());
+    });
 
-    const handleFilter = (event) =>{
-       const searchWord = event.target.value
-       const newFilter = data.filter((value) => {
-        return value.city.toLowerCase().includes(searchWord.toLowerCase())
-       })
-
-       if(searchWord === ""){
-        setFilteredData([])
-       }else{
-        setFilteredData(newFilter)
-       }
+    if (searchWord === "") {
+      setFilteredData([]);
+    } else {
+      setFilteredData(newFilter);
     }
+  };
 
-    return(
-        <div className="search">
-            <div className="form-outline">
-                <label></label>
-                <input type="text"  className="form-control" placeholder={placeholder} onChange={handleFilter}/>
-            </div>
-            {filteredData.length != 0 && 
-            <div className="dataResults">
-                {filteredData.map((value, key) =>{
-                    return(
-                        <a href=""><p className="dataItem">{value.restName}</p></a>
-                    )
-                })}
-            </div>
-            }
+  return (
+    <div className="search">
+      <div className="form-outline">
+        <label></label>
+        <input
+          type="text"
+          className="form-control"
+          placeholder={placeholder}
+          onChange={handleFilter}
+        />
+      </div>
+      {filteredData.length != 0 && (
+        <div className="dataResults">
+          {filteredData.map((value, key) => {
+            return (
+              <p
+                className="dataItem"
+                onClick={() => {
+                  axios
+                    .get("http://localhost:3000/restaurants/" + value._id)
+                    .then((response) => {
+                      let newRestaurant = props.restaurants.map(
+                        (restaurant) => {
+                          return restaurant._id == value._id
+                            ? restaurant
+                            : value;
+                        }
+                      );
+                      props.setRestaruants(newRestaurant);
+                    });
+                }}
+              >
+                {value.restName}
+              </p>
+            );
+          })}
         </div>
-    )
-}
+      )}
+    </div>
+  );
+};
 
-export default Search
+export default Search;
